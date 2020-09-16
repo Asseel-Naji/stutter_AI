f1d28ac9790e09b2b953bd32a03d5def9d8154b0
Asseel Naji <asseel.naji96@gmail.com>
2020-09-16 03:51:25 +0300

    more renaming

--- fix_names_script.py
+++ fix_names_script.py
@@ -1,9 +1,9 @@
 # who got time to fix  the names manually?
 import os
 
-for file in os.listdir("/home/ravingmad/Desktop/Work/amazonthon/stutter_AI/dataset/stutters"):
+for file in os.listdir("/home/ravingmad/Desktop/Work/amazonthon/stutter_AI/dataset_stereo/stutters"):
     fixed_name = file.replace("inst.wav","sttr.wav")
 
 
-    os.rename("/home/ravingmad/Desktop/Work/amazonthon/stutter_AI/dataset/stutters/"+file, "/home/ravingmad/Desktop/Work/amazonthon/stutter_AI/dataset/stutters/"+fixed_name)
+    os.rename("/home/ravingmad/Desktop/Work/amazonthon/stutter_AI/dataset_stereo/stutters/"+file, "/home/ravingmad/Desktop/Work/amazonthon/stutter_AI/dataset_stereo/stutters/"+fixed_name)
 
